103c655ae30b09fdf817b69e30480867188f746f
Daniel Martinez <dmartinez@itexico.net>
2017-06-09 11:26:36 -0500

    Add cancel functionality

--- app/src/main/java/com/example/danny/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/danny/notekeeper/NoteActivity.java
@@ -21,6 +21,7 @@ public class NoteActivity extends AppCompatActivity {
     private EditText mTextNoteTile;
     private EditText mTextNoteText;
     private int mNotePosition;
+    private boolean mIsCancelling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +51,13 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        saveNote();
+        if(mIsCancelling){
+            if(mIsNewNote){
+                DataManager.getInstance().removeNote(mNotePosition);
+            }
+        }else{
+            saveNote();
+        }
     }
 
     private void saveNote() {
@@ -102,6 +109,9 @@ public class NoteActivity extends AppCompatActivity {
         if (id == R.id.action_send_mail) {
             sendEmail();
             return true;
+        }else if( id == R.id.action_cancel){
+            mIsCancelling = true;
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
